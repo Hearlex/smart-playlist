c36aea477b07388f0a1f6f18a958e1b08a39553e
Hegedüs Armand Alex <hearlex@gmail.com>
2023-05-06 23:11:04 +0200

    changes to reflect docs

--- src/pages/api/updateMusic.js
+++ src/pages/api/updateMusic.js
@@ -3,34 +3,37 @@ export default function handler(req, res) {
 
     const sqlite3 = require('sqlite3').verbose();
 
-    // open database
-    let db = new sqlite3.Database('./db/test.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the file SQlite database.');
-    });
-
-    db.run(`UPDATE music SET artist = (?), title = (?), tags = (?) WHERE id = (?)`, [body.artist, body.title, body.tags, body.id], function(err) {
+    try {
+        // open database
+        let db = new sqlite3.Database('./db/test.db', sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
-            return console.log(err.message);
+            return console.error(err.message);
         }
-        // get the last insert id
-        console.log(`A row has been modified with rowid ${this.lastID}`);
-    });
-
-    // close the database connection
-    db.close((err) => {
-    if (err) {
-        return console.error(err.message);
+        console.log('Connected to the file SQlite database.');
+        });
+    
+        db.run(`UPDATE music SET artist = (?), title = (?), tags = (?) WHERE id = (?)`, [body.artist, body.title, body.tags, body.id], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been modified with rowid ${this.lastID}`);
+        });
+    
+        // close the database connection
+        db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+        });
+    } catch (error) {
+        console.log(error);
     }
-    console.log('Close the database connection.');
-    });
-
-    console.log('name: ', body.name);
-    if (body.name) {
-        res.status(200).json({ name: body.name })
+    
+    if (body.artist && body.title && body.tags && body.id) {
+        res.status(200).json({ artist: body.artist, title: body.title, tags: body.tags, id: body.id })
     } else {
-        res.status(400).json({ message: 'Missing name' })
+        res.status(400).json({ message: 'Missing parameters' })
     }
 }
